2865f431d353959a8c4f87ee08a89b5cd7912064
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 13:23:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,10 @@
 class Triangle
+  attr_accessor :length1, :length2, :length3
 
   def initialize(length1, length2, length3)
-    
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
 
 end
