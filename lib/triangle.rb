64ae5ba6725b605c47dee2fd50c5c96e4d1af5f9
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 13:31:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,20 @@ class Triangle
   end
 
   def kind
-    type = ''
-    if
+    type = nil
+    #check if valid triangle
+    if !((@length1 + @length2 > @length3 && @length2 + @length3 > @length1 && @length1 + @length3 > @length2) && (@length1 > 0 && @length2 > 0 && @length3 > 0))
+      raise TriangleError
+    else
+      if @length1 == @length2 && @length2 == @length3
+        type = :equilateral
+      elsif @length1 == @length2 || @length2 == @length3
+        type = :isosceles
+      else
+        type = :scalene
+      end
+    end
+
   end
 
   class TriangleError < StandardError
